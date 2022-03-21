a8f5253d17773fa5ce42e1b19f1ebe262cc0f624
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 22:20:00 +0000

    Create LeapYearTest.java

---
+++ test/LeapYearTest.java
@@ -0,0 +1,14 @@
+package leapyear.test;
+
+import leapyear.base.display.LeapYearDisplay;
+
+public class LeapYearTest {
+
+    public static void main(String[] args) {
+
+        LeapYearDisplay test = new LeapYearDisplay();
+        
+    }
+
+
+}
